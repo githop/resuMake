5543824881280476af790dd5ebd45455f3ec7b84
githop <githop@gmail.com>
2014-01-23 11:35:37 -0500

    Create code.js

---
+++ code.js
@@ -0,0 +1,68 @@
+function doGet() {
+  var html = HtmlService.createHtmlOutputFromFile('page')
+      .setSandboxMode(HtmlService.SandboxMode.NATIVE)
+      .setTitle('ResuMake Test');
+  return html
+}
+
+function processForm(form) {  
+  var formInput = {
+    firstName:   form.canFirst,
+    lastName:    form.canLast,
+    sizzle:      form.sizzle,
+    clinDoc:     form.clinDoc,
+    stork:       form.stork,
+    beacon:      form.beacon,
+    anesthesia:  form.anesthesia,
+    empName:     form.empName,
+    empYears:    form.empYears,
+    description: form.description,
+    school:      form.school,
+    eduYears:    form.eduYears,
+    degree:      form.degree
+  };
+  
+  var certs = [formInput.clinDoc, formInput.stork, formInput.beacon, formInput.anesthesia];
+  
+  var gDoc   = DocumentApp.create("Resume for: " + formInput.firstName + " " + formInput.lastName);
+  var doc    = gDoc.getBody();
+  var img    = UrlFetchApp.fetch('http://i.imgur.com/s6wnamr.jpg');
+  
+  var title  = {};
+  var h1     = {};
+  var plain  = {};
+  
+  title[DocumentApp.Attribute.FONT_SIZE]   = 21;
+  
+  h1[DocumentApp.Attribute.FONT_SIZE]      = 16; 
+ 
+  plain[DocumentApp.Attribute.FONT_SIZE]   = 11;
+  
+
+  //Logo
+  doc.setMarginTop(25).appendImage(img).setHeight(50).setWidth(150); 
+  
+  doc.appendParagraph(formInput.firstName + " " + formInput.lastName)
+     .setAlignment(DocumentApp.HorizontalAlignment.CENTER).setAttributes(title);
+  
+  //Candidate Name
+  doc.appendParagraph("Candidate Summary").setAttributes(h1);
+  
+  //Sizzle
+  doc.appendParagraph(formInput.sizzle).setAttributes(plain);
+  
+  //Epic Certs
+  doc.appendParagraph("Epic Certifications").setAttributes(h1); 
+ 
+  for (var i = 0; i < certs.length; i++) {
+    if (certs[i] != null) {
+      var listItem = doc.appendListItem(certs[i]).setGlyphType(DocumentApp.GlyphType.BULLET).setAttributes(plain);
+    }
+  }
+  
+  doc.appendParagraph("Employment History").setAttributes(h1);
+  doc.appendTable([[formInput.empName, formInput.empYears]]).setBorderWidth(0);
+  
+}
+
+
